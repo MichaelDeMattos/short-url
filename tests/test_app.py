da968f32bf0b0dad7ed0712c4c96b752ee2953bc
Michael Ortiz <57331815+MichaelDeMattos@users.noreply.github.com>
2023-03-29 16:48:43 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ import os.path
 import requests
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-host = "http://165.232.154.205:5000"
+host = "http://flask_api:5000"
 
 def test_fetch_short_url():
     """ Consumer Rest API /api/short_url with Methods [GET, POST, PUT, DELETE] """
